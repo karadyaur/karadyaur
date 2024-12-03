905f372f5738e43dcadb094c53d79f1437143183
Oleksandr Karadiaur <alex.karadyaur@outlook.com>
2024-12-03 14:08:23 +0100

    some fixes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",  // <=== enables static exports
+  basePath: '/karadyauran',
   reactStrictMode: true,
 };
 
